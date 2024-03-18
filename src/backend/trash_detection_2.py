36f7e7a7ea5131f88767ad8fd8d745fc7c6b29e7
Leland Sion <lelandsion@gmail.com>
2024-03-17 18:44:12 -0700

    moved to image detection branch

--- src/backend/trash_detection_2.py
+++ src/backend/trash_detection_2.py
@@ -40,7 +40,7 @@ def image_detection(file_name):
         'image_path': 'trash_image_7.jpg', 'prediction_type': 'ObjectDetectionModel'}],
         'image': {'width': '800', 'height': '500'}}
     """
-    img = cv2.imread(file_name)
+    # img = cv2.imread(file_name)
 
     # train model for trash detection on custom dataset
     # model =  YOLO('yolov8m.pt')
@@ -53,7 +53,7 @@ def image_detection(file_name):
 
     result = model.predict(file_name, save=True, show=True)
 
-    input()
+    # input()
 
     # Store Json Results
     results = json.loads(result[0].tojson())
@@ -99,5 +99,22 @@ print(image_detection('ocean_trash_image_1.jpg'))
 print(image_detection('ocean_trash_image_2.jpg'))
 print(image_detection('ocean_trash_image_3.jpg'))
 """
-print(image_detection('ocean_trash_image_4.jpg'))
+# print(image_detection('trash_image_26.jpg'))
+# print(image_detection('trash_image_27.jpg'))
+# print(image_detection('trash_image_28.jpg'))
+# print(image_detection('trash_image_29.jpg'))
+# print(image_detection('trash_image_30.jpg'))
+# print(image_detection('trash_image_31.jpg'))
+# print(image_detection('trash_image_32.jpg'))
+# print(image_detection('trash_image_33.jpg'))
+# print(image_detection('trash_image_34.jpg'))
+# print(image_detection('trash_image_35.jpg'))
+# print(image_detection('trash_image_36.jpg'))
+# print(image_detection('trash_image_37.jpg'))
+
+print(image_detection('trash_image_38.jpg'))
+print(image_detection('trash_image_39.jpg'))
+print(image_detection('trash_image_40.jpg'))
+
+
 
